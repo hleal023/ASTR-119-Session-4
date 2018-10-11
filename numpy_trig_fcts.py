b11fce78323be1533f4f8dd6b5213313195d72b7
hleal023 <43651670+hleal023@users.noreply.github.com>
2018-10-11 11:16:20 -0700

    Add files via upload

---
+++ numpy_trig_fcts.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+x = 1.0 					#define floats
+y = 2.0
+
+#trig
+print(np.sin(x))			#sin(x)
+print(np.cos(x))			#cos(x)
+print(np.tan(x))			#tan(x)
+print(np.arcsin(x))			#arcsin(x)
+print(np.arccos(x))			#arccos(x)
+print(np.arctan(x))			#acrtan(x)
+print(np.arctan2(x,y))		#arctan(x/y))
+print(np.rad2deg(x))		#convert rad to deg
+
+#hyperbolic fct
+print(np.sinh(x))			#sinh(x)
+print(np.cosh(x))			#cosh(x)
+print(np.tanh(x))			#tanh(x)
+print(np.arcsinh(x))		#arcsinh(x)
+print(np.arccosh(x))		#arccosh(x)
+print(np.arctanh(x))		#arctanh(x)
+
